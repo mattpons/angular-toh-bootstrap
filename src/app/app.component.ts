4b1814701e85c23dbacbc4a9cfab4066057dd799
Matt Pons <mrp67@drexel.edu>
2016-11-09 23:06:01 -0500

    Finish tutorial part 1
    Initial bootstrap styling added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './shared/hero.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Bootstrap Heroes';
+  hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+};
 }
