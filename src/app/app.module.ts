7e84cbbb14ac82dfe3ff10eb4890827726c91c24
Matt Pons <mrp67@drexel.edu>
2016-12-06 23:43:15 -0500

    Add hero search
    This completes the tour of heroes tutorial features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
+import './rxjs-extensions';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
-import { InMemoryDataService }  from './shared/in-memory-data.service';
-
+import { InMemoryDataService } from './shared/in-memory-data.service';
 import { HeroFetchService } from './shared/hero-fetch.service';
 
 import { CarouselModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
@@ -23,7 +25,8 @@ import { CarouselModule } from 'ng2-bootstrap/ng2-bootstrap';
         AppComponent,
         HeroDetailComponent,
         HeroListComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
     imports: [
         BrowserModule,
